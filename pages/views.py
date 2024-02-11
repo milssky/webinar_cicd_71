595026e98bcd16931f95d7d42cecc507d95db137
Alexey Potapov <2furius@gmail.com>
2024-02-11 21:18:39 +0300

    Change message

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,6 @@ from django.http import JsonResponse
 
 
 def index_view(request):
-    return JsonResponse({"wazzup": "hacker????!!"})
+    return JsonResponse({"wazzup": "hacker!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"})
 
 
